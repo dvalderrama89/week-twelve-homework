1a30b483127110c0e17412a1e9f7bddfe6ad7a4e
David Valderrama <dvalderrama08@gmail.com>
2021-05-30 13:36:11 -0700

    Added db connections

--- cms.js
+++ cms.js
@@ -1,6 +1,26 @@
 const inquirer = require("inquirer");
-const connection = require("mysql");
 const cTable = require('console.table');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port, if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: 'root',
+  database: 'employee_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
+});
 
 console.table([
   {
