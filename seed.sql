a9ee756475f14f5f9816e695ea145d85ed5318ac
David Valderrama <dvalderrama08@gmail.com>
2021-06-05 14:11:01 -0700

    Fixed the seed to include drop and adding of db/tables

--- seed.sql
+++ seed.sql
@@ -1,17 +1,21 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
 -- Department data
-INSERT INTO department (name) VALUES ("Engineering");
-INSERT INTO department (name) VALUES ("Art");
-
--- Employee data
-INSERT INTO employee (
-first_name, last_name, role_id, manager_id)
-VALUES ("Hiyo", "Kaychan", 1, 1);
-
-INSERT INTO employee (
-first_name, last_name, role_id, manager_id)
-VALUES ("Retina", "J. Artist", 2, 2);
+CREATE TABLE department (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30)
+);
 
 -- Role data
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL(10, 2),
+department_id INT
+);
+
 INSERT INTO role (
 title, salary, department_id)
 VALUES ("Senior Game Developer",
@@ -21,3 +25,23 @@ INSERT INTO role (
 title, salary, department_id)
 VALUES ("Art Lead",
 90000.00, 2);
+
+INSERT INTO department (name) VALUES ("Engineering");
+INSERT INTO department (name) VALUES ("Art");
+
+-- Employee data
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT
+);
+
+INSERT INTO employee (
+first_name, last_name, role_id, manager_id)
+VALUES ("David", "Test", 1, 1);
+
+INSERT INTO employee (
+first_name, last_name, role_id, manager_id)
+VALUES ("James", "J. Artist", 2, 2);
